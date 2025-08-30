7abc13e64c110a421202c63d07535fc468dbc046
mtt <theuz_9@hotmail.com>
2025-08-30 19:21:55 -0300

    refactor: remove unused ListViewsFiles action and clean up project file

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,17 +5,6 @@ namespace GloboClimaFrontend.Controllers
 {
     public class HomeController : Controller
     {
-        [HttpGet]
-        [Route("/diagnostic/views")]
-        public IActionResult ListViewsFiles()
-        {
-            var viewsPath = Path.Combine(Directory.GetCurrentDirectory(), "Views");
-            if (!Directory.Exists(viewsPath))
-                return Content("Views directory not found.");
-            var files = Directory.GetFiles(viewsPath, "*.*", SearchOption.AllDirectories);
-            return Content(string.Join("\n", files));
-        }
-
         public IActionResult Index()
         {
             return View();
